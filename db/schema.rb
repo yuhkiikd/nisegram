adf7bb8cf1cff915f15735e9a940f7e55f2135c1
yuhkiikd <yuhki.ikd@gmail.com>
2019-09-29 14:14:35 +0000

    2019092906_ユーザー登録機能追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_29_020921) do
+ActiveRecord::Schema.define(version: 2019_09_29_132702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2019_09_29_020921) do
   create_table "images", force: :cascade do |t|
     t.text "image"
     t.text "content"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
