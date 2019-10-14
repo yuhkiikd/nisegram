fa7846e9e56d5e60eb99ac82a325f4b2eabede03
yuhkiikd <yuhki.ikd@gmail.com>
2019-10-14 11:15:18 +0000

    2019101403_bootstrap設置・cssでスタイリング

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,16 +6,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
+      flash[:success] = 'ログインしました'
       redirect_to user_path(user.id)
     else
-      flash.now[:danger] = "ログインに失敗しました"
-      render :new
+      flash[:danger] = "ログインに失敗しました"
+      redirect_to new_session_path
     end
   end
 
   def destroy
     session.delete(:user_id)
-    flash[:notice] = "ログアウトしました"
+    flash[:success] = "ログアウトしました"
     redirect_to new_session_path
   end
 end
